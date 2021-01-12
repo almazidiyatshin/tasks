1553d4f6145e511f809f9482ca90ea02ccecfff0
Almaz Idiyatshin <dev@tshin.ru>
2021-01-12 13:15:15 +0300

    Fix task-1

--- task-1.js
+++ task-1.js
@@ -1,6 +1,7 @@
 // Написать функцию, которая будет возвращать сумму элементов массива.
 
 const arr = [1, 2, -1, 3];
-const getSum = (array) => array.reduce((acc, value) => acc + value, 0);
 
-console.log(getSum(arr));
+arr.sum = () => arr.reduce((acc, value) => acc + value, 0);
+
+console.log(arr.sum()); // 5

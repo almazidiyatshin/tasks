29d7c8b8d42908a38e5bc38b58f89111a4116b60
Almaz Idiyatshin <dev@tshin.ru>
2021-01-13 11:57:47 +0300

    Fix task-1

--- task-1.js
+++ task-1.js
@@ -2,6 +2,8 @@
 
 const arr = [1, 2, -1, 3];
 
-arr.sum = () => arr.reduce((acc, value) => acc + value, 0);
+Array.prototype.sum = function sum() {
+  return this.reduce((acc, value) => acc + value, 0);
+};
 
 console.log(arr.sum()); // 5
